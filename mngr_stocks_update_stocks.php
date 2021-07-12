3b7a5c8efdf18042b120a7ab209cf916aa213f43
optimuspnj <contact@prabhashlk.com>
2021-07-12 15:34:32 +0530

    Fix: Bind parameters error

--- mngr_stocks_update_stocks.php
+++ mngr_stocks_update_stocks.php
@@ -11,7 +11,7 @@
     #Script for update stock details
     $sql = "UPDATE `stock` SET `stock_item_name` = ?, `stock_brand_name` = ?, `stock_supplier_id` = ?, `stock_man_year` = ?, `stock_wsp` = '?, `stock_quantity` = ? WHERE `stock`.`stock_id` = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssisdi", $itemName, $brandName, $supId, $manYear, $wholeSalep, $quantity, $itemId);
+    $stmt->bind_param("ssisdii", $itemName, $brandName, $supId, $manYear, $wholeSalep, $quantity, $itemId);
     $itemName = $_POST["itemName"];
     $brandName = $_POST["brandName"];
     $supId = $_POST["supId"];
